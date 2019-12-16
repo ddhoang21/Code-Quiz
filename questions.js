daf39f12b13cf4d7e7e813aed6027f5e4c6105c1
DDH <ddhoang1993@gmail.com>
2019-12-15 21:16:20 -0800

    HTML layout fix, new js file

---
+++ questions.js
@@ -0,0 +1,27 @@
+var allQuestions = [
+    {
+        title: "Commonly used data types DO NOT include:",
+        choices: ["strings", "booleans", "alerts", "numbers"],
+        answer: 2
+    },
+    {
+        title: "The condition in an if / else statement is enclosed within ____.",
+        choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+        answer: 2
+    },
+    {
+        title: "Arrays in JavaScript can be used to store ______",
+        choice: ["numbers and strings", "other arrays", "booleans", "all of the above"],
+        answer: 3
+    },
+    {
+        title: "String values must be enclosed within ______ when being assigned to variables.",
+        choices: ["commas", "curly brackets", "quotes", "parentheses"],
+        answer: 2
+    },
+    {
+        title: "A very useful tool during development and debugging for printing content to the debugger is",
+        choices: ["JavaScript", "Terminal", "for loops", "console log"],
+        answer: 4
+    }
+]
--- script.js
+++ script.js
@@ -1,32 +1,32 @@
-var quizBox = document.getElementById("quiz");
-var startBtn = document.getElementById("start");
+var startBtn = document.getElementById("start-button");
 var submitBtn = document.getElementById("submit");
-var resultsBox = document.getElementById("results");
 
-var questions = [
-    {
-        title: "Commonly used data types DO NOT include:",
-        choices: ["strings", "booleans", "alerts", "numbers"],
-        answer: "alerts"
-    },
-    {
-        title: "The condition in an if / else statement is enclosed within ____.",
-        choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-        answer: "parentheses"
-    },
-    {
-        title: "Arrays in JavaScript can be used to store ______",
-        choice: ["numbers and strings", "other arrays", "booleans", "all of the above"],
-        answer: "all of the above"
-    },
-    {
-        title: "String values must be enclosed within ______ when being assigned to variables.",
-        choices: ["commas", "curly brackets", "quotes", "parentheses"],
-        answer: "quotes"
-    },
-    {
-        title: "A very useful tool during development and debugging for printing content to the debugger is",
-        choices: ["JavaScript", "Terminal", "for loops", "console log"],
-        answer: "console log"
+var questionBox = document.getElementById("question-container");
+var box0 = document.getElementById("choice0");
+var box1 = document.getElementById("choice1");
+var box2 = document.getElementById("choice2");
+var box3 = document.getElementById("choice3");
+
+document.getElementById("choice0").style.visibility = "hidden";
+document.getElementById("choice1").style.visibility = "hidden";
+document.getElementById("choice2").style.visibility = "hidden";
+document.getElementById("choice3").style.visibility = "hidden";
+document.getElementById("submit-button").style.visibility = "hidden";
+
+startBtn.addEventListener("click", displayQuestion);
+
+function displayQuestion() {
+    document.getElementById("text").style.visibility = "hidden";
+    document.getElementById("start-button").style.visibility = "hidden";
+    document.getElementById("choice0").style.visibility = "visible";
+    document.getElementById("choice1").style.visibility = "visible";
+    document.getElementById("choice2").style.visibility = "visible";
+    document.getElementById("choice3").style.visibility = "visible";
+    for (i = 0; i < allQuestions.length; i++) {
+        questionBox.innerHTML = allQuestions[0].title;
+        box0.innerHTML = allQuestions[0].choices[0];
+        box1.innerHTML = allQuestions[0].choices[1];
+        box2.innerHTML = allQuestions[0].choices[2];
+        box3.innerHTML = allQuestions[0].choices[3];
     }
-]
+}
