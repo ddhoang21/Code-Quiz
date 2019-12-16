31b51d6e634e84a69272666d4427a39b1daadcc8
DDH <ddhoang1993@gmail.com>
2019-12-16 14:00:42 -0800

    display first question when clicking start

--- questions.js
+++ questions.js
@@ -2,26 +2,26 @@ var allQuestions = [
     {
         title: "Commonly used data types DO NOT include:",
         choices: ["strings", "booleans", "alerts", "numbers"],
-        answer: 2
+        answer: "alerts"
     },
     {
         title: "The condition in an if / else statement is enclosed within ____.",
         choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-        answer: 2
+        answer: "parentheses"
     },
     {
         title: "Arrays in JavaScript can be used to store ______",
         choice: ["numbers and strings", "other arrays", "booleans", "all of the above"],
-        answer: 3
+        answer: "all of the above"
     },
     {
         title: "String values must be enclosed within ______ when being assigned to variables.",
         choices: ["commas", "curly brackets", "quotes", "parentheses"],
-        answer: 2
+        answer: "quotes"
     },
     {
         title: "A very useful tool during development and debugging for printing content to the debugger is",
         choices: ["JavaScript", "Terminal", "for loops", "console log"],
-        answer: 4
+        answer: "console log"
     }
 ]
--- script.js
+++ script.js
@@ -7,21 +7,25 @@ var box1 = document.getElementById("choice1");
 var box2 = document.getElementById("choice2");
 var box3 = document.getElementById("choice3");
 
+var questionAnswer = document.getElementById("answers");
+
 document.getElementById("choice0").style.visibility = "hidden";
 document.getElementById("choice1").style.visibility = "hidden";
 document.getElementById("choice2").style.visibility = "hidden";
 document.getElementById("choice3").style.visibility = "hidden";
 document.getElementById("submit-button").style.visibility = "hidden";
 
-startBtn.addEventListener("click", displayQuestion);
+startBtn.addEventListener("click", startQuiz);
 
-function displayQuestion() {
+function startQuiz() {
+
     document.getElementById("text").style.visibility = "hidden";
     document.getElementById("start-button").style.visibility = "hidden";
     document.getElementById("choice0").style.visibility = "visible";
     document.getElementById("choice1").style.visibility = "visible";
     document.getElementById("choice2").style.visibility = "visible";
     document.getElementById("choice3").style.visibility = "visible";
+
     for (i = 0; i < allQuestions.length; i++) {
         questionBox.innerHTML = allQuestions[0].title;
         box0.innerHTML = allQuestions[0].choices[0];
@@ -29,4 +33,8 @@ function displayQuestion() {
         box2.innerHTML = allQuestions[0].choices[2];
         box3.innerHTML = allQuestions[0].choices[3];
     }
+    box2.addEventListener("click", function() {
+       questionAnswer.innerHTML = "Correct!";
+      });
+
 }
